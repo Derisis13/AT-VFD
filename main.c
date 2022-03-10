9a326de094d867dca3a92ebcf1178441d714adf4
Derisis13 <email.felhasznalasra@gmail.com>
2022-03-10 09:37:02 +0100

    LGPL compliance

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/*
+ * AT-VFD - example project
+ * Copyright (C) 2022  László Párkányi
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
 #define F_CPU 8000000UL
 #define DISPLAY_DIGITS 12
 
@@ -19,7 +39,7 @@ int main ()
     {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
   PT6302_startup ();
   set_ports (1, 1);
-  set_digits (12);
+  set_digits (DISPLAY_DIGITS);
   set_duty (7);
   set_ADRAM (0, digitstates, DISPLAY_DIGITS);
   set_CGRAM (0,custom_chars, 8);
